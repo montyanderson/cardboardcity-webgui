be651790555ca7a707a3a7131487fccdef18ad91
Monty Anderson <monty.linux@gmail.com>
2015-06-18 19:45:32 +0100

    moved to less and started suggestions

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var fs = require("fs"),
+    less = require("less"),
     http = require("http"),
-    express = require("express");
+    express = require("express"),
     socketio = require("socket.io");
+
 
 var app = express();
 app.use(express.static("public"));
+
+app.get("/style.css", function(req, res) {
+    fs.readFile(__dirname + "/public/style.less", function(err, data) {
+
+        less.render(data.toString()).then(function(output) {
+            res.write(output.css);
+            res.end();
+        }, function(error) {
+            console.log(error);
+        });
+        
+    });
+});
 
 var server = http.createServer(app);
 var io = socketio(server);
